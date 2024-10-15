4b4eb4379d52660f60b2e39986c80ee5082b54a9
“Chavda <chavdasandip2908@gmail.com>
2024-10-15 23:22:14 +0530

    chage time

--- index.js
+++ index.js
@@ -81,35 +81,41 @@ async function createWhatsAppGroup(phoneNumbers) {
 
     // Type each phone number into the search input and press Enter
     for (let phoneNumber of phoneNumbers) {
-        await page.waitForSelector('#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x9desvi.x1gz9zih.xsag5q8.x1b9tyad.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x150wa6m > div > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.x1q0g3np.x1cy8zhl > input', { timeout: 3000 });
+        await page.waitForSelector('#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x9desvi.x1gz9zih.xsag5q8.x1b9tyad.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x150wa6m > div > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.x1q0g3np.x1cy8zhl > input', { timeout: 20000 });
         await page.type('#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x9desvi.x1gz9zih.xsag5q8.x1b9tyad.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x150wa6m > div > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.x1q0g3np.x1cy8zhl > input', phoneNumber.toString());
         // await delay(1000);
         try {
-            await page.waitForSelector("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div[role='button']", { timeout: 10000 });
+
+            await page.waitForSelector("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div[role='button']", { timeout: 15000 });
             await page.click("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div[role='button']");
         } catch (e) {
             try {
-                await page.waitForSelector("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div > div > div > div > div[role='button'] > div", { timeout: 7000 });
+                await page.waitForSelector("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div > div > div > div > div[role='button'] > div", { timeout: 900 });
                 await page.click("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div > div > div > div > div[role='button'] > div");
             } catch (e2) {
 
-                // try {
-                //     // Check for the element that indicates the number is not registered
-                //     // await page.waitForSelector("#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x1n2onr6.x1n2onr6.x1iyjqo2.xs83m0k.x1r8uery.x6ikm8r.x1odjw0f.x150wa6m > div > div > span._ao3e", { timeout: 60000 });
-
-                //     // Clear the input box
                 await page.evaluate(() => {
                     document.querySelector('#app > div > div.two._aigs > div._aigu > div._aohf._aigv._aigw._aigx > span > div > span > div > div > div.x9desvi.x1gz9zih.xsag5q8.x1b9tyad.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x150wa6m > div > div > div.x1c4vz4f.xs83m0k.xdl72j9.x1g77sc7.x78zum5.xozqiw3.x1oa3qoh.x12fk4p8.xeuugli.x2lwn1j.x1nhvcw1.x1q0g3np.x1cy8zhl > input').value = '';
                 });
+                // Check for the element that indicates the number is not registered
+
+
+                // .then(async element => {
+                //     // Get the value or inner text of the element
+                // })
+                // .catch(error => {
+                //     console.error('Error:', error);
+                //     return null;  // Return null if the element is not found or any error occurs
+                // });
+                // Clear the input box
+
 
                 console.log('Number is not registered, cleared input.');
-                // } catch (er) {
-                //     console.log(er);
-                //     console.log("Error detected while checking registration status.");
-                // }
+
             }
         }
         // await delay(1000);
+
     }
 
     // Click the "Next" button after all numbers are entered
